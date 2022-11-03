9799b8eb2688842e71303dc4a67633d7244abbc5
Jose Gabriel <gabrielneves13@hotmail.com>
2022-11-03 08:10:15 -0300

    arquivos finais do desfio 3

---
+++ ex07(Condicionais).js
@@ -0,0 +1,9 @@
+let idade = 70
+//Condicionais devem estar entre parênteses
+if (idade < 16){
+    console.log("Você não pode votar")
+} else if (idade > 18 && idade <= 60){
+    console.log("Você já pode votar")
+} else {
+    console.log("Voto é opcional")
+}
