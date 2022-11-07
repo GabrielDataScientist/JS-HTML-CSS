a9db2ccf9c6dbdb39a358590b7ad3ce0faaf9bad
Jose Gabriel <gabrielneves13@hotmail.com>
2022-11-07 11:17:09 -0300

    Arrays and objects

---
+++ HTML/JS/ex14(Metodos).js
@@ -0,0 +1,14 @@
+/*
+    São proriedades que funcionam como funções
+    Aceitam parâmetros
+
+
+*/
+
+let nome = "Gabriel"
+
+//Propriedade
+console.log(nome.length)
+
+//Método
+console.log(nome.toUpperCase())
